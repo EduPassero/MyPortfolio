8f1fff5428b3fde1ae83d90c6fab2e4f956262aa
EduPassero <edupassero47@gmail.com>
2023-12-12 01:04:42 -0300

    Correções no CSS. Descobri que algumas mudanças anteriores foram perdidas.

--- wwwroot/js/jsFunctions.js
+++ wwwroot/js/jsFunctions.js
@@ -1,10 +1,18 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
+    
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById('nav').classList.remove('nav_active');
+        document.getElementById('nav').classList.add('nav_active');
+        document.getElementById('nav').style.top = "0";
     } else {
         document.getElementById('nav').classList.add('nav_active');
+        document.getElementById('nav').style.top = "-150px";
     }
+
+    if (currentScrollPos == 0) {
+        document.getElementById('nav').classList.remove('nav_active');
+    }
+
     prevScrollpos = currentScrollPos;
 }
